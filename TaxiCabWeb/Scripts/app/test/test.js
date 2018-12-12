0df200585c00a220be6ecfc81e2af0b1d379f7be
Daniel Orlan <ddanielorlano@gmail.com>
2018-12-12 17:24:22 -0500

    Added karma and jasmin

---
+++ TaxiCabWeb/Scripts/app/test/test.js
@@ -0,0 +1,23 @@
+﻿describe("taxi fare test", function () {
+    var $rootScope;
+    var $controller;
+    beforeEach(module("appModule"));
+    beforeEach(inject(function ($injector) {
+
+        $rootScope = $injector.get('$rootScope');
+        $controller = $injector.get('$controller');
+        $scope = $rootScope.$new();
+
+    }));
+    beforeEach(inject(function ($controller) {
+        taxiFareRateController = $controller("taxiFareRateController", {$scope: $rootScope});
+
+    }));
+
+    it("Should say hello", function () {
+        $scope.rideMinute = 1;
+        $scope.getFare();
+        expect($scope.taxiCabRateResponseModel.Success).toBe(false);
+    });
+
+});
